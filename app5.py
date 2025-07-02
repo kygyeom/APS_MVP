b9b23bc302872c7b3398aef59bddb0bfff923376
kygyeom <99728367+kygyeom@users.noreply.github.com>
2025-07-02 20:30:31 +0900

    Update app5.py

--- app5.py
+++ app5.py
@@ -24,20 +24,15 @@ components.html("""
     </script>
 """, height=0)
 
-GA_MEASUREMENT_ID = "G-SQL1CS5F2E"  # GA4 ID
-
-ga_code = f"""
-<!-- Google tag (gtag.js) -->
-<script async src="https://www.googletagmanager.com/gtag/js?id={GA_MEASUREMENT_ID}"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){{dataLayer.push(arguments);}}
-  gtag('js', new Date());
-  gtag('config', '{GA_MEASUREMENT_ID}');
-</script>
-"""
-components.html(ga_code, height=0)
-
+st.components.v1.html("""
+<!-- Google Tag Manager -->
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id=GTM-K3QDM4K6'+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-K3QDM4K6');</script>
+<!-- End Google Tag Manager -->
+""", height=0)
 
 import random
 
